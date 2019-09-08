db46b36af6af4184c4e56ddd530681460dc15478
rssxda10 <riancrazzy2@gmail.com>
2019-09-08 01:43:35 -0300

    msm8916-samsung: Saucy Potatoes bringup [2/2]

--- potato.mk
+++ potato.mk
@@ -7,10 +7,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product-if-exists, vendor/aosp/config/common_full_phone.mk)
-
-# Inherit some common AEX stuff.
-$(call inherit-product-if-exists, vendor/aosp/common.mk)
+$(call inherit-product-if-exists, vendor/potato/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -25,7 +22,6 @@ TARGET_GAPPS_ARCH := arm
 
 # J5 is a low-end device, include minimal and Go apps
 IS_GO_VERSION := true
-TARGET_MINIMAL_APPS := true
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
